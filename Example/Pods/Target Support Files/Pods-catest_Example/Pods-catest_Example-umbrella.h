eeae8b1cba633994409a2ab64d39324a1a3ae426
wuzhx <wdgxzhongxiang@163.com>
2016-06-24 19:08:00 +0800

    uodate .gitignore

--- Example/Pods/Target Support Files/Pods-catest_Example/Pods-catest_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_catest_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_catest_ExampleVersionString[];
-
--- Example/Pods/Target Support Files/catest/catest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_catest : NSObject
-@end
-@implementation PodsDummy_catest
-@end
